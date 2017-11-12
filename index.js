28cfc6fd1a21073aebe82274fd9ca616ab55ce2b
Helen Liutongco <bellpickle@gmail.com>
2017-11-12 04:16:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ class BoardMember{
   veto(){
     return "No, I must disagree";
   }
+  approve(){
+    return "You can do that!"
+  }
   doCharity() {
     return "I like to help people."
   }
-  
+
 }
